c41e9014a22e1e89c5494e4cf46079eee9a9dff1
Muhammad Irtiza <muhammad.irtiza751@gmail.com>
2024-10-16 13:23:13 +0500

    implement sigin form validation & submition logic

--- apps/web/src/app/(auth)/sign-in/page.tsx
+++ apps/web/src/app/(auth)/sign-in/page.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { Button } from "@/components/ui/button";
 import {
   Card,
@@ -7,31 +9,88 @@ import {
   CardTitle,
 } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
 import { Separator } from "@radix-ui/react-separator";
 import { FcGoogle } from "react-icons/fc";
 import Link from "next/link";
+import { z } from "zod";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+
+const signinSchema = z.object({
+  email: z.string().email(),
+  password: z.string().min(6).max(150),
+});
+
+type Signin = z.infer<typeof signinSchema>;
 
 export default function SigninPage() {
+  const form = useForm<Signin>({
+    resolver: zodResolver(signinSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+
+  const onSubmit = (data: Signin) => {
+    console.log(data);
+  };
+
   return (
     <Card className="max-w-lg w-full mx-auto">
       <CardHeader>
         <CardTitle className="text-2xl">Welcome back</CardTitle>
       </CardHeader>
       <CardContent>
-        <form className="space-y-4">
-          <div className="space-y-2">
-            <Label>Enter your email</Label>
-            <Input placeholder="example@email.com" type="email" />
-          </div>
-          <div className="space-y-2">
-            <Label>Enter your password</Label>
-            <Input placeholder="****************" type="password" />
-          </div>
-          <Button size="lg" className="w-full">
-            Sign in
-          </Button>
-        </form>
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
+            <FormField
+              control={form.control}
+              name="email"
+              render={({ field }) => (
+                <FormItem className="space-y-2">
+                  <FormLabel className="text-black">Enter email</FormLabel>
+                  <FormControl>
+                    <Input
+                      {...field}
+                      placeholder="example@email.com"
+                      type="email"
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="password"
+              render={({ field }) => (
+                <FormItem className="space-y-2">
+                  <FormLabel className="text-black">Enter password</FormLabel>
+                  <FormControl>
+                    <Input
+                      {...field}
+                      placeholder="*************"
+                      type="password"
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <Button size="lg" className="w-full">
+              Sign in
+            </Button>
+          </form>
+        </Form>
       </CardContent>
       <Separator className="my-2" />
       <CardFooter className="flex-col gap-3">
