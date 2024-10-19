cef0f2832650c0879bd2fd9b2c159fa8a503a40c
Muhammad Irtiza <muhammad.irtiza751@gmail.com>
2024-10-19 09:57:01 +0500

    Initialize and setup tailwind & typescript configs

---
+++ packages/tailwind-config/tailwind.config.ts
@@ -0,0 +1,13 @@
+const config = {
+  theme: {
+    extend: {
+      container: {
+        center: true,
+        padding: "1rem",
+      },
+    },
+  },
+  plugins: [],
+};
+
+export default config;
