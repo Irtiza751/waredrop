f66365b1fc87e47e9d9fe526a12555ddf3b354fb
Muhammad Irtiza <muhammad.irtiza751@gmail.com>
2024-12-14 23:22:26 +0500

    Establist dabase connection

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 // configs
 import envSchema from './config/env.validation';
 import appConfig from './config/app.config';
@@ -14,6 +15,20 @@ import dbConfig from './config/db.config';
       envFilePath: '.env.dev',
       load: [appConfig, dbConfig],
       validationSchema: envSchema,
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        port: configService.get('db.port'),
+        username: configService.get('db.user'),
+        password: configService.get('db.password'),
+        host: configService.get('db.host'),
+        database: configService.get('db.name'),
+        autoLoadEntities: configService.get('db.autoLoadEntities'),
+        synchronize: configService.get('db.synchronize'),
+      }),
     }),
   ],
   controllers: [AppController],
--- apps/backend/src/config/db.config.ts
+++ apps/backend/src/config/db.config.ts
@@ -1,6 +1,6 @@
 import { registerAs } from '@nestjs/config';
 
-export default registerAs('database', () => ({
+export default registerAs('db', () => ({
   host: process.env.DATABASE_HOST || 'localhost',
   port: parseInt(process.env.DATABASE_PORT) || 5432,
   user: process.env.DATABASE_USER,
