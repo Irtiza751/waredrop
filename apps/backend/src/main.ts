f88641c0ed923f71ca2af0b12317530f5f738c8f
Muhammad Irtiza <muhammad.irtiza751@gmail.com>
2024-12-14 23:01:25 +0500

    Add necessary dependancies

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -3,7 +3,11 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  /**
+   * Setting the global prefix api/endpoint
+   */
   app.setGlobalPrefix('api');
+
   await app.listen(4000);
 }
 
