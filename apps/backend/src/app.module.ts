12b8e13b02e103d8d55742f8f3cc8fd750e010dc
Muhammad Irtiza <muhammad.irtiza751@gmail.com>
2024-12-14 14:37:52 +0500

    setup config and config validation

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+// configs
+import { ConfigModule } from '@nestjs/config';
+import envSchema from './config/env.validation';
+import appConfig from './config/app.config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env.dev',
+      load: [appConfig],
+      validationSchema: envSchema,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
