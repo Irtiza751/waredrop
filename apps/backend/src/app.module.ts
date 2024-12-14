23c02a33bb4d874361cde3561499e2cc42696d65
Muhammad Irtiza <muhammad.irtiza751@gmail.com>
2024-12-14 22:48:07 +0500

    added the database config variables

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -1,17 +1,18 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-// configs
 import { ConfigModule } from '@nestjs/config';
+// configs
 import envSchema from './config/env.validation';
 import appConfig from './config/app.config';
+import dbConfig from './config/db.config';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
       envFilePath: '.env.dev',
-      load: [appConfig],
+      load: [appConfig, dbConfig],
       validationSchema: envSchema,
     }),
   ],
--- apps/backend/src/config/env.validation.ts
+++ apps/backend/src/config/env.validation.ts
@@ -5,4 +5,13 @@ export default Joi.object({
     .valid('development', 'production', 'test', 'provision')
     .default('development'),
   PORT: Joi.number().default(4000),
+  // database
+  DATABASE_HOST: Joi.string().required(),
+  DATABASE_PORT: Joi.number().default(5432),
+  DATABASE_USER: Joi.string().required(),
+  DATABASE_PASSWORD: Joi.string().required(),
+  DATABASE_NAME: Joi.string().required(),
+  // development
+  DATABASE_SYNC: Joi.boolean().default(false),
+  DATABASE_AUTOLOAD: Joi.boolean().default(false),
 });
