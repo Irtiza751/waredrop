5597f2ae1ab7b8b73d86cfcb62be198b78750c0c
Muhammad Irtiza <muhammad.irtiza751@gmail.com>
2024-12-25 13:51:17 +0500

    Fix dynamic breadcrumb issues

--- apps/web/src/components/shop-header.tsx
+++ apps/web/src/components/shop-header.tsx
@@ -1,13 +1,10 @@
+"use client";
+import { usePathname } from "next/navigation";
 import breadcrumbs from "@/utils/breadcrumbs";
-import { headers } from "next/headers";
+import Link from "next/link";
 
 export default function ShopHeader({ title }: { title?: string }) {
-  const headerList = headers();
-  const url =
-    headerList.get("x-forwarded-url") || headerList.get("referer") || "";
-  const path = new URL(url).pathname;
-
-  console.log({ url, path });
+  const path = usePathname();
 
   return (
     <section className="h-56 container bg-stone-100 rounded-lg my-5">
@@ -15,7 +12,19 @@ export default function ShopHeader({ title }: { title?: string }) {
         <h2 className="text-5xl max-w-4xl font-semibold text-center leading-normal text-black uppercase">
           {title?.replace("-", " ") || "Shop Page"}
         </h2>
-        <p className="capitalize">{breadcrumbs(path || "") ?? "Home / Shop"}</p>
+        <ul className="capitalize flex gap-1">
+          {breadcrumbs(path).map((crumb) => (
+            <>
+              <li
+                key={crumb.name}
+                className="flex gap-1 hover:underline hover:text-blue-700"
+              >
+                <Link href="/">{crumb.name}</Link>
+              </li>
+              {!crumb.last ? <span>/</span> : null}
+            </>
+          ))}
+        </ul>
       </div>
     </section>
   );
