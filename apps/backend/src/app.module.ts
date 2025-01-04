55336a5dfc2dbe93874180ff2e39c265981355fe
Muhammad Irtiza <muhammad.irtiza751@gmail.com>
2025-01-04 16:59:41 +0500

    Add the seperate jwt config file

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -9,13 +9,14 @@ import { AuthModule } from './auth/auth.module';
 import envSchema from './config/env.validation';
 import appConfig from './config/app.config';
 import dbConfig from './config/db.config';
+import jwtConfig from './config/jwt.config';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
       envFilePath: '.env.dev',
-      load: [appConfig, dbConfig],
+      load: [appConfig, dbConfig, jwtConfig],
       validationSchema: envSchema,
     }),
     TypeOrmModule.forRootAsync({
