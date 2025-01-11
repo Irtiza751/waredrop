64c60764e5bf5ff7c07d280b90e24f51b3104ac5
Muhammad Irtiza <muhammad.irtiza751@gmail.com>
2025-01-11 14:10:26 +0500

    Mark the creater user handler as public

--- apps/backend/src/user/user.controller.ts
+++ apps/backend/src/user/user.controller.ts
@@ -1,11 +1,13 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { UserService } from './providers/user.service';
 import { CreateUserDto } from './dtos/user.dto';
+import { MarkPublic } from 'src/auth/decorators/public.decorator';
 
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
+  @MarkPublic()
   @Post('create-user')
   createUser(@Body() user: CreateUserDto) {
     return this.userService.createUser(user);
