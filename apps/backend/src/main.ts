56205eb4f414df9e6e537ebeeef7db126ff79574
Muhammad Irtiza <muhammad.irtiza751@gmail.com>
2025-01-11 14:04:56 +0500

    enable cors & mark root route of hello as public

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -2,8 +2,13 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 
+const allowedOrigins = ['http://localhost:3000'];
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: allowedOrigins,
+  });
   /**
    * Setting the global prefix api/endpoint
    */
