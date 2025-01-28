8584003ae616854354052538dd561493e1b29c62
Muhammad Irtiza <muhammad.irtiza751@gmail.com>
2025-01-28 12:54:14 +0500

    implement the route protection middleware

--- apps/web/src/middleware.ts
+++ apps/web/src/middleware.ts
@@ -1,12 +1,24 @@
-import { NextResponse } from "next/server";
-import type { NextRequest } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
+import { cookies } from "next/headers";
 
-// This function can be marked `async` if using `await` inside
-export function middleware(request: NextRequest) {
-  return NextResponse.redirect(new URL("/home", request.url));
+const publicRoutes = ["/sign-in", "/sign-up"];
+
+export default async function middleware(req: NextRequest) {
+  const path = req.nextUrl.pathname;
+  const isPublicRoute = publicRoutes.includes(path);
+
+  const cookie = cookies().get("sessionid")?.value;
+
+  // if the cookie exist, don't let the user go to the auth pages.
+  if (isPublicRoute && cookie) {
+    return NextResponse.redirect(new URL("/", req.nextUrl));
+  }
+
+  console.log({ path });
+  return NextResponse.next();
 }
 
-// See "Matching Paths" below to learn more
+// Routes Middleware should not run on
 export const config = {
-  matcher: "/about/:path*",
+  matcher: ["/((?!api|_next/static|_next/image|.*\\.png$).*)"],
 };
