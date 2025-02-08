f7447e6e06a462680ef5406ea2c7dcd1db74ad25
Muhammad Irtiza <muhammad.irtiza751@gmail.com>
2025-02-08 16:48:46 +0500

    implement the create product dto

--- apps/backend/src/products/dtos/create-product.dto.ts
+++ apps/backend/src/products/dtos/create-product.dto.ts
@@ -1,13 +1,9 @@
+import { ProductStatus } from '../enums/product-status.enum';
+
 export class CreateProductDto {
-  name: string; // name of the product
-  createdBy: number; // id of the user who created this product
-  image: string; // image url string
-  description: string; // product description
+  title: string;
+  description: string;
   price: number;
-  discount: number;
   quantity: number;
-  status: string; // in stock or out of stock;
-  rating: number;
-  sizes: string[]; // xl, lg, or xxl
-  colors: string[];
+  status: ProductStatus;
 }
--- apps/backend/src/products/product.controller.ts
+++ apps/backend/src/products/product.controller.ts
@@ -1,10 +1,11 @@
 import { Controller, Post } from '@nestjs/common';
+import { CreateProductDto } from './dtos/create-product.dto';
 
 @Controller('products')
 export class ProductController {
   constructor() {}
   @Post('/create')
-  createProduct() {
-    return { msg: 'Product created' };
+  createProduct(createProductDto: CreateProductDto) {
+    return { msg: 'Product created', product: createProductDto };
   }
 }
