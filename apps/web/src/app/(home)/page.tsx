491f4163d5038d0eec1d68e3f13281f38c903301
Muhammad Irtiza <muhammad.irtiza751@gmail.com>
2025-03-02 17:52:44 +0500

    improve her section ui design

--- apps/web/src/app/(home)/page.tsx
+++ apps/web/src/app/(home)/page.tsx
@@ -1,17 +1,37 @@
-// import Image from "next/image";
 import { Button } from "@waredrop/ui";
-// import summer from "@/assets/images/summer.jpg";
-// import winter from "@/assets/images/winter.jpg";
+import summer from "@/assets/images/summer.jpg";
+import winter from "@/assets/images/winter.jpg";
 import Link from "next/link";
 import { waredropApi } from "@/api/waredrop.api";
 import { Product as IProduct } from "@/types/product-interface";
 import ProductList from "@/components/product-list";
+import Image from "next/image";
 
 export default async function Home() {
   const { data } = await waredropApi.get<IProduct[]>("/products");
 
   return (
     <div>
+      <section className="container flex items-center gap-2 mt-12">
+        <div className="flex-1">
+          <h2 className="text-5xl font-semibold max-w-2xl leading-normal">
+            Seamless shopping for stylish looks that don&apos;t break the bank.
+          </h2>
+        </div>
+        <div className="flex-1 grid grid-cols-2 gap-4">
+          <Image
+            src={summer}
+            alt="Two each of gray, white, and black shirts laying flat."
+            className="block rounded-xl"
+          />
+          <Image
+            src={winter}
+            alt="Two each of gray, white, and black shirts laying flat."
+            className="block rounded-xl mt-8"
+          />
+        </div>
+      </section>
+
       <section>
         <div className="container">
           <h3 className="text-center lg:text-4xl text-3xl mb-5 mt-16 max-w-lg mx-auto">
--- apps/web/src/components/navbar.tsx
+++ apps/web/src/components/navbar.tsx
@@ -2,9 +2,9 @@
 
 import { FiShoppingBag, FiSearch, FiUser } from "react-icons/fi";
 import {
-  Avatar,
-  AvatarFallback,
-  AvatarImage,
+  // Avatar,
+  // AvatarFallback,
+  // AvatarImage,
   Button,
   DropdownMenu,
   DropdownMenuContent,
@@ -18,7 +18,7 @@ import Link from "next/link";
 import Image from "next/image";
 import logo from "@/assets/images/logo.png";
 import useAuthStore from "@/store/auth-store";
-import { extractUserName } from "@/utils/extract-username";
+// import { extractUserName } from "@/utils/extract-username";
 import { clearSession } from "@/actions/clear-session";
 import { useRouter } from "next/navigation";
 
@@ -60,29 +60,33 @@ export default function Navbar() {
             <FiSearch size={20} />
           </Button>
           {user ? (
-            <DropdownMenu>
-              <DropdownMenuTrigger asChild>
-                <Button size="icon" variant="link">
-                  <FiUser size={20} />
-                </Button>
-              </DropdownMenuTrigger>
-              <DropdownMenuContent className="w-52" align="end">
-                <DropdownMenuLabel>Account</DropdownMenuLabel>
-                <DropdownMenuSeparator />
-                <DropdownMenuItem>Profile</DropdownMenuItem>
-                <DropdownMenuItem>Settings</DropdownMenuItem>
-                <DropdownMenuItem onClick={signOut}>Sign out</DropdownMenuItem>
-              </DropdownMenuContent>
-            </DropdownMenu>
-          ) : null}
-          <Button size="icon" variant="link">
-            <FiShoppingBag size={20} />
-          </Button>
-          {!user ? (
+            <>
+              <DropdownMenu>
+                <DropdownMenuTrigger asChild>
+                  <Button size="icon" variant="link">
+                    <FiUser size={20} />
+                  </Button>
+                </DropdownMenuTrigger>
+                <DropdownMenuContent className="w-52" align="end">
+                  <DropdownMenuLabel>Account</DropdownMenuLabel>
+                  <DropdownMenuSeparator />
+                  <DropdownMenuItem>Profile</DropdownMenuItem>
+                  <DropdownMenuItem>Settings</DropdownMenuItem>
+                  <DropdownMenuItem onClick={signOut}>
+                    Sign out
+                  </DropdownMenuItem>
+                </DropdownMenuContent>
+              </DropdownMenu>
+              <Button size="icon" variant="link" className="gap-2">
+                <FiShoppingBag size={20} />
+                <span>0</span>
+              </Button>
+            </>
+          ) : (
             <Button asChild variant="link">
               <Link href="/sign-in">Sign In</Link>
             </Button>
-          ) : null}
+          )}
         </div>
       </nav>
     </header>
