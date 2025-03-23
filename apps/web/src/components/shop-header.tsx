ffa1be8852225f57bf2c3a54927b708c495ca15d
Muhammad Irtiza <muhammad.irtiza751@gmail.com>
2025-03-23 15:02:12 +0500

    Refactor ShopHeader and breadcrumbs utility for improved breadcrumb navigation and styling

--- apps/web/src/components/shop-header.tsx
+++ apps/web/src/components/shop-header.tsx
@@ -2,6 +2,7 @@
 import { usePathname } from "next/navigation";
 import breadcrumbs from "@/utils/breadcrumbs";
 import Link from "next/link";
+import React from "react";
 
 export default function ShopHeader({ title }: { title?: string }) {
   const path = usePathname();
@@ -12,17 +13,14 @@ export default function ShopHeader({ title }: { title?: string }) {
         <h2 className="text-5xl max-w-4xl font-semibold text-center leading-normal text-black uppercase">
           {title?.replace("-", " ") || "Shop Page"}
         </h2>
-        <ul className="capitalize flex gap-1">
+        <ul className="capitalize flex gap-2">
           {breadcrumbs(path).map((crumb) => (
-            <>
-              <li
-                key={crumb.name}
-                className="flex gap-1 hover:underline hover:text-blue-700"
-              >
-                <Link href="/">{crumb.name}</Link>
+            <React.Fragment key={crumb.name}>
+              <li className="flex gap-1 hover:underline hover:text-blue-700">
+                <Link href={crumb.href}>{crumb.name}</Link>
               </li>
-              {!crumb.last ? <span>/</span> : null}
-            </>
+              {!crumb.last ? <span className="text-stone-400">/</span> : null}
+            </React.Fragment>
           ))}
         </ul>
       </div>
--- apps/web/src/utils/breadcrumbs.ts
+++ apps/web/src/utils/breadcrumbs.ts
@@ -1,11 +1,11 @@
 export default function breadcrumbs(path: string) {
-  const chunks = path.split("/");
+  const chunks = path.split("/"); // path chunks array
 
-  return chunks
-    .map((item) => item.replaceAll("-", " "))
-    .map((item, i) => {
-      const last = chunks.length - 1 === i;
-      const result = { name: item ? item : "Home", last };
-      return result;
-    });
+  return chunks.map((chunk, i) => {
+    const item = chunk.replaceAll("-", " ");
+    const last = chunks.length - 1 === i;
+    const href = chunks.slice(0, i + 1).join("/") || "/";
+    const result = { name: item ? item : "Home", last, href };
+    return result;
+  });
 }
